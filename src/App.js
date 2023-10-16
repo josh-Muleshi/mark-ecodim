afd84bd2f7fcbd6d77e16757f4196e5e8d6391b8
joshmuleshi <jmuleshi2@gmail.com>
2023-10-16 10:17:25 +0200

    first feature done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown'
 import './App.css';
 
 function App() {
+
+  const [input, setInput] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea autoFocus className="textarea" value={input} onChange={(e) => setInput(e.target.value)}/>
+      <ReactMarkdown children={input} className="markdown"/>
     </div>
   );
 }
